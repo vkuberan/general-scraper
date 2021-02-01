f03a66d9e47ff0a28d2fbe535c0dbd8f36151a84
Velmurugan Kuberan <vkuberan@gmail.com>
2021-02-01 09:34:51 +0530

    Common purpose scrape libs

--- gs_lib/wiki.py
+++ gs_lib/wiki.py
@@ -1,7 +1,2 @@
 from gs_lib import general
 import time
-
-print("Hello from Wiki")
-input("Press any key to continue....")
-general.clear_screen()
-time.sleep(3)
--- wiki-entry.py
+++ wiki-entry.py
@@ -1,1 +1,7 @@
+from gs_lib import general
 from gs_lib import wiki
+
+
+link_src = 'https://en.wikipedia.org/wiki/Leonardo_DiCaprio'
+actor = general.fetch_data(link_src, 'data/wiki/leonardo-dicaprio.html')
+print(actor)
