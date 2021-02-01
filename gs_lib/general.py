c64d6b1805ce647b5a0ef0b919c5417c018c5810
Velmurugan Kuberan <vkuberan@gmail.com>
2021-02-01 09:29:46 +0530

    Common purpose scrape libs

---
+++ gs_lib/general.py
@@ -0,0 +1,92 @@
+import os
+import platform
+import subprocess
+import json
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from bs4 import BeautifulSoup
+
+# seconds
+DEFAULT_TIMEOUT = 5
+retry_strategy = Retry(
+    total=5,
+    backoff_factor=1,
+    status_forcelist=[429, 500, 502, 503, 504],  # http://httpstat.us/
+    method_whitelist=["HEAD", "GET", "OPTIONS"]
+)
+
+
+# To set default timeout parameter for our scrapper
+# GS => General Scraper
+# Refer: https://findwork.dev/blog/advanced-usage-python-requests-timeouts-retries-hooks/#request-hooks
+class GSTimeOutHTTPAdapter(HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.timeout = DEFAULT_TIMEOUT
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+
+        return super().send(request, **kwargs)
+
+
+def clear_screen():
+    command = "cls" if platform.system().lower() == "windows" else "clear"
+    return subprocess.call(command, shell=True)
+
+
+def print_char_under_string(msg, char='*', newline='\n\n'):
+    msg += "\n" + (char * len(msg))
+    print(msg, end=newline)
+
+
+def create_related_dirs(project_dirs):
+    # create 2 separate directories to save html and the scraped data
+    for dirname, dirpath in project_dirs.items():
+        # check weather the dir exists, if not create new one
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+
+
+# Fetch that url and save it locally and can we used for future use
+def fetch_data(link_source, html_file_to_save):
+
+    html_source = ''
+    try:
+        with open(html_file_to_save, 'rb') as hs:
+            html_source = hs.read().decode("UTF-16")
+            print_char_under_string(
+                "Fetching info from the crawled file.", '-', '\n')
+    except Exception as e:
+        errno, errmsg = e.args
+        errmsg = "Error({}): {}, Creating new file {}.".format(
+            errno, errmsg, html_file_to_save)
+        print_char_under_string(errmsg, '*', '\n\n')
+        print_char_under_string(
+            "Fetching data from the server using request.", '-', '\n')
+
+        try:
+            adapter = GSTimeOutHTTPAdapter(
+                max_retries=retry_strategy, timeout=5)
+            http = requests.Session()
+            http.mount("https://", adapter)
+            http.mount("http://", adapter)
+
+            response = http.get(link_source)
+            # print(response.headers)
+
+            html_source = response.text
+            with open(html_file_to_save, mode='w', encoding='UTF-16') as f:
+                f.write(response.text)
+
+        except Exception as e:
+            print(e)
+
+    return html_source
---
+++ gs_lib/wiki.py
@@ -0,0 +1,7 @@
+from gs_lib import general
+import time
+
+print("Hello from Wiki")
+input("Press any key to continue....")
+general.clear_screen()
+time.sleep(3)
